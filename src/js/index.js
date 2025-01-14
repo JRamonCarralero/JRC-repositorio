46d0e77d9fd05da43deb4f33d151026191c6ec81
JRamonCarralero <grukduz@gmail.com>
2025-01-14 21:11:31 +0100

    lista de la compra y pokedex

--- src/js/index.js
+++ src/js/index.js
@@ -1,69 +1,143 @@
-//  variables
+//  Constants
 const item = document.getElementById('item')
-const qty = document.getElementById('qty')
-const price = document.getElementById('price')
+const itemQty = document.getElementById('qty')
+const itemPrice = document.getElementById('price')
 const listTotal = document.getElementById('list-total')
 const tbody = document.getElementById('list-tbody')
 
-let shoppingList = []
-console.log(shoppingList)
+// Define product items
+const PRODUCTS = {
+    MILK: 'leche',
+    FRUIT: 'fruta',
+    MEAT: 'carne',
+    WATER: 'agua',
+    BREAD: 'pan'
+}
 
-// Añade un elemento a la lista y llama a la función que pinta la tabla
+let shoppingList = []
+
+/**
+ * function addItem()
+ * Add new item to shoppingList
+ * Call drawTable function
+ * @returns 
+ */
 function addItem() {  
     if (!item.value) {
         alert('Debe rellenar el campo artículo')
         return
     }
-    if (!qty.value) {
-        alert('Debe rellenar el campo cantidad')
-        return
+    let qty = 0
+    let price = 0
+    let article = item.value
+    switch (article) {
+        case PRODUCTS.MILK:
+            qty = 6
+            price = 2
+            break;
+        case PRODUCTS.FRUIT:
+            qty = 3
+            price = 1
+            break;
+        case PRODUCTS.MEAT:
+            qty = 2
+            price = 11
+            break;
+        case PRODUCTS.WATER:
+            qty = 6
+            price = 1
+            break;
+        case PRODUCTS.BREAD:
+            qty = 2
+            price = 1
+            break;
+        default:
+            break;
     }
-    if (!price.value) {
-        alert('Debe rellenar el campo precio')
-        return
+
+    if (itemQty.value) {
+        qty = Number(itemQty.value)
+    }
+    if (itemPrice.value) {
+        price = Number(itemPrice.value)
     }
     let newItem = {
-        item: item.value,
-        qty: qty.value,
-        price: price.value
+        item: article,
+        qty: qty,
+        price: price
     }
     shoppingList.push(newItem)
     item.value = ''
-    qty.value = ''
-    price.value = ''
-    console.log(shoppingList)
+    itemQty.value = ''
+    itemPrice.value = ''
     drawTable()
 }
 
-// Limpipa lista de elementos y la tabla que los muestra
+/**
+ * function clearList()
+ * Change shoppingList to an empty array
+ * Clear tbody from table 'list-table'
+ */
 function clearList() {
     shoppingList = []
     console.log(shoppingList)
     tbody.innerHTML = ''
-    listTotal.textContent = '0€'
+    listTotal.innerText = '0 €'
 }
 
-// Rellena la tabla con la lista de elementos
+/**
+ * function drawTable()
+ * Draw the table 'list-table' with the shoppingList's articles
+ */
 function drawTable() {
     tbody.innerHTML = ''
     let totalAmount = 0
     shoppingList.forEach((el, index) => {
         let total = el.qty * el.price
+        /* literal template 
         tbody.innerHTML += `
             <tr class="bb1">
                 <td>${el.item}</td>
                 <td>${el.qty}</td>
-                <td>${el.price}€</td>
-                <td>${total}€</td>
+                <td>${el.price} €</td>
+                <td>${total} €</td>
                 <td><button onclick="deleteItem(${index})">borrar</button></td>
             </tr>
         `
+        */
+        let row = document.createElement('tr')
+        row.classList.add('bb1')
+        let cellItem = document.createElement('td')
+        cellItem.innerText = el.item
+        row.appendChild(cellItem)
+        let cellQty = document.createElement('td')
+        cellQty.innerText = el.qty
+        row.appendChild(cellQty)
+        let cellPrice = document.createElement('td')
+        cellPrice.innerText = el.price + '€'
+        row.appendChild(cellPrice)
+        let cellTotal = document.createElement('td')
+        cellTotal.innerText = total + '€'
+        row.appendChild(cellTotal)
+        let cellBtn = document.createElement('td')
+        let delBtn = document.createElement('button')
+        delBtn.onclick = () => deleteItem(index)
+        delBtn.innerText = 'Borrar'
+        cellBtn.appendChild(delBtn)
+        row.appendChild(cellBtn)
+        tbody.appendChild(row)
+        
         totalAmount += total
     })
-    listTotal.textContent = totalAmount + '€'
+    listTotal.innerText = totalAmount + ' €'
 }
 
-// Elimina el elemento de la lista y llama a la función que pinta la tabla
+/**
+ * function deleteItem(index)
+ * Delete an article from the shoppingList
+ * Call drawTable function
+ * @param {number} index 
+ */
 function deleteItem(index) {
     shoppingList.splice(index, 1)
     drawTable()
---
+++ src/js/pokemon.js
@@ -0,0 +1,99 @@
+/**
+ * Define some pokemons for test
+ */
+let pokemons = [
+    {
+        name: 'Bulbasaur',
+        id: '0001',
+        type: ['planta', 'veneno']
+    },
+    {
+        name: 'Caterpie',
+        id: '0002',
+        type: ['bicho']
+    },
+    {
+        name: 'Metapod',
+        id: '0003',
+        type: ['bicho']
+    },
+    {
+        name: 'Butterfree',
+        id: '0004',
+        type: ['bicho', 'volador']
+    },
+    {
+        name: 'Weedle',
+        id: '0005',
+        type: ['bicho', 'veneno']
+    },
+    {
+        name: 'Kakuna',
+        id: '0006',
+        type: ['bicho', 'veneno']
+    },
+    {
+        name: 'Beedrill',
+        id: '0007',
+        type: ['bicho', 'veneno']
+    },
+    {
+        name: 'Pidgey',
+        id: '0008',
+        type: ['normal', 'volador']
+    }
+]
+
+/**
+ * function searchPokemon
+ * ToDo:
+ * Search pokemons by id or by name
+ * Call drawPokemons function
+ */
+function searchPokemon() {
+    let input = document.getElementById('search').value
+    if (isNaN(Number(input))) {
+        console.log('buscar por nombre')
+    } else {
+        console.log('buscar por numero')
+    }
+
+    drawPokemons(pokemons)
+}
+
+/**
+ * function drawPokemons
+ * Draw the list of Pokemons
+ * @param {Array} pok 
+ * pok is a pokemons object's array
+ */
+function drawPokemons(pok) {
+    const pList = document.getElementById('list')
+    pList.innerHTML = ''
+    for (let i=0; i<pok.length; i++){
+        let elist = document.createElement('li')
+        let img = document.createElement('img')
+        img.src = 'https://placehold.co/150x150'
+        img.alt = pok[i].name
+        elist.appendChild(img)
+        let idText =  document.createElement('p')
+        idText.innerText = 'Nº ' + pok[i].id
+        elist.appendChild(idText)
+        let nameText =  document.createElement('h3')
+        nameText.innerText = pok[i].name
+        elist.appendChild(nameText)
+        pok[i].type.forEach(item => {
+            let typeText =  document.createElement('span')
+            typeText.innerText = item
+            typeText.classList.add(item)
+            elist.appendChild(typeText)
+        });
+        pList.appendChild(elist)
+    }
+}
+
+/**
+ * call drawPokemons when the page is load
+ * @returns 
+ */
+window.onload = () => drawPokemons(pokemons)
